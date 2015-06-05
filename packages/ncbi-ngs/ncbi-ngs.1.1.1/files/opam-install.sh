79d560e5dc612b62a0524b16f1d8778bd639372e
Ashish Agarwal <agarwal1975@gmail.com>
2015-06-05 14:35:04 -0400

    ncbi-ngs: fix directories
    Rename install directory to _install, instead of foo. Also specify
    —build-prefix because the default pollutes your home directory.

--- packages/ncbi-ngs/ncbi-ngs.1.1.1/files/opam-install.sh
+++ packages/ncbi-ngs/ncbi-ngs.1.1.1/files/opam-install.sh
@@ -5,8 +5,8 @@ LIB_DIR=$1
 SHARE_DIR=$2
 
 mkdir $LIB_DIR
-cp -Rp ngs-sdk/foo/lib64/* $LIB_DIR/
-cp -Rp ngs-sdk/foo/include/ngs $LIB_DIR/
+cp -Rp ngs-sdk/_install/lib64/* $LIB_DIR/
+cp -Rp ngs-sdk/_install/include/ngs $LIB_DIR/
 
 mkdir $SHARE_DIR
-cp -Rp ngs-sdk/foo/share/* $SHARE_DIR/
+cp -Rp ngs-sdk/_install/share/* $SHARE_DIR/
