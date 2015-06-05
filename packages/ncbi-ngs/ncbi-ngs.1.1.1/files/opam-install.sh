1bbb89b7b35eca75205ae5c5bdd33530eb18532d
Ashish Agarwal <agarwal1975@gmail.com>
2015-06-05 15:28:01 -0400

    ncbi-ngs, ncbi-vdb: change install path
    Dependent package sra-tools assumes libraries of these packages are at
    a path prefixed with lib64/, so cannot remove that when we install.

--- packages/ncbi-ngs/ncbi-ngs.1.1.1/files/opam-install.sh
+++ packages/ncbi-ngs/ncbi-ngs.1.1.1/files/opam-install.sh
@@ -5,7 +5,7 @@ LIB_DIR=$1
 SHARE_DIR=$2
 
 mkdir $LIB_DIR
-cp -Rp ngs-sdk/_install/lib64/* $LIB_DIR/
+cp -Rp ngs-sdk/_install/lib64 $LIB_DIR/
 cp -Rp ngs-sdk/_install/include/ngs $LIB_DIR/
 
 mkdir $SHARE_DIR
--- packages/ncbi-vdb/ncbi-vdb.2.5.0/files/opam-install.sh
+++ packages/ncbi-vdb/ncbi-vdb.2.5.0/files/opam-install.sh
@@ -4,5 +4,5 @@ set -e
 LIB_DIR=$1
 
 mkdir $LIB_DIR
-cp -Rp _install/lib64/* $LIB_DIR/
+cp -Rp _install/lib64 $LIB_DIR/
 cp -Rp _install/include/ncbi-vdb $LIB_DIR/
