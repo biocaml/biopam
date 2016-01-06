2002db01f7065714b17ea9eeaf7732e9347ed44d
Philippe Veber <philippe.veber@gmail.com>
2016-01-06 16:25:50 +0100

    fixed spades 3.6.2

--- packages/spades/spades.3.6.2/files/opam-build.sh
+++ packages/spades/spades.3.6.2/files/opam-build.sh
@@ -8,5 +8,8 @@ find bin -type f | gawk '{ printf "  \"%s\"\n",$1}' >> spades.install
 echo "]" >> spades.install
 
 echo "share: [" >> spades.install
-find share/spades -type f | gawk '{ printf "  \"%s\"\n",$1}' >> spades.install
+find share/spades -type f \
+    | sed "s|share/spades/||g" \
+    | gawk '{ printf "  \"share/spades/%s\" { \"%s\" }\n",$1,$1}' \
+    >> spades.install
 echo "]" >> spades.install
